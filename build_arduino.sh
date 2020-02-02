161546a1c7a85488ff6e80c5f0a77664cf4f1afc
Adam Demuri <adam.demuri@gmail.com>
2020-02-02 11:24:21 -0700

    Add option to build Arduino for only a single board

--- build_arduino.sh
+++ build_arduino.sh
@@ -2,14 +2,26 @@
 
 set -euo pipefail
 
+if [ "${1-}" == "-h" ] || [ "${1-}" == "--help" ]; then
+  echo "usage: build_arduino.sh <board>"
+  exit 0
+fi
+
+BUILD_ALL=true
+BOARD_TO_BUILD=
+if [ "${1-}" != "" ]; then
+  BUILD_ALL=false
+  BOARD_TO_BUILD="$1"
+fi
+
 BOARDS="--board teensy31 --board adafruit_feather_m0 --board esp32dev"
 LIBC_NEEDED_BOARDS="--board uno"
 
-
-platformio ci ${BOARDS?} --lib=. examples/debounce
-platformio ci ${BOARDS?} --lib=. examples/exponential-moving-average
-platformio ci ${BOARDS?} --lib=. examples/median-filter
-
-platformio ci ${LIBC_NEEDED_BOARDS?} --project-option="lib_deps=ArduinoSTL" --lib=. examples/debounce
-platformio ci ${LIBC_NEEDED_BOARDS?} --project-option="lib_deps=ArduinoSTL" --lib=. examples/exponential-moving-average
-platformio ci ${LIBC_NEEDED_BOARDS?} --project-option="lib_deps=ArduinoSTL" --lib=. examples/median-filter
+for dir in examples/*; do
+  if [ "$BUILD_ALL" == "true" ]; then
+    platformio ci ${BOARDS?} --lib=. "${dir?}"
+    platformio ci ${LIBC_NEEDED_BOARDS?} --project-option="lib_deps=ArduinoSTL" --lib=. "${dir?}"
+  else
+    platformio ci --board "${BOARD_TO_BUILD?}" --lib=. "${dir?}"
+  fi
+done
