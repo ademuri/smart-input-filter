f351391bbc61be4774eadd7d6ca1c22aec77bacd
Adam DeMuri <adam.demuri@gmail.com>
2023-03-04 10:14:07 -0700

    Fix edge filter and add example

---
+++ examples/edge-filter/edge-filter.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <edge-filter.h>
+
+constexpr uint8_t kInputPin = 0;
+constexpr int32_t kEdgeThreshold = 10;
+
+EdgeFilter filter(filter_functions::ForAnalogRead<kInputPin>(), /*alpha=*/191);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(kInputPin, INPUT);
+  // The filter will work better if it runs at a regular interval
+  filter.SetMinRunInterval(1);
+}
+
+void loop() {
+  filter.Run();
+
+  // Depending on the signal, these will print multiple times for each rising or
+  // falling edge.
+  if (filter.Rose(kEdgeThreshold)) {
+    Serial.println("Rising edge detected");
+  } else if (filter.Fell(kEdgeThreshold)) {
+    Serial.println("Falling edge detected");
+  }
+}
--- src/edge-filter.h
+++ src/edge-filter.h
@@ -36,33 +36,3 @@ class EdgeFilter : public Filter<int32_t, int32_t> {
   // losing precision.
   static constexpr int32_t kValueScale = 10;
 };
-
-EdgeFilter::EdgeFilter(
-    typename Filter<int32_t, int32_t>::ReadFromSensorType read_from_sensor,
-    uint8_t alpha)
-    : Filter<int32_t, int32_t>(read_from_sensor),
-      input_filter_(read_from_sensor, alpha) {}
-
-int32_t EdgeFilter::DoRun() {
-  input_filter_.Run();
-
-  int32_t current_time = millis();
-  int32_t period = current_time - prev_value_time_;
-  if (period == 0) {
-    period = 1;
-  }
-  const int32_t current_value = input_filter_.GetFilteredValue();
-  slope_ = ((current_value - prev_value_) * kValueScale) / period;
-
-  prev_value_ = current_value;
-  prev_value_time_ = current_time;
-  return current_value;
-}
-
-void EdgeFilter::LogState() {
-  Serial.print(Convert_(sensor_value_));
-  Serial.print(" ");
-  Serial.print(Convert_(GetFilteredValue()));
-  Serial.print(" ");
-  Serial.println(slope_);
-}
